d81720df176a921fbf7c480214ae7e5f4c1c760d
MicroFish91 <mwfisher91@gmail.com>
2022-09-29 13:32:21 -0700

    base

---
+++ Scripts/install.ps1
@@ -0,0 +1,5 @@
+# https://learn.microsoft.com/en-us/powershell/azure/install-az-ps?view=azps-8.3.0
+
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_modules?view=powershell-7.2
+
+Install-Module -Name Az 
